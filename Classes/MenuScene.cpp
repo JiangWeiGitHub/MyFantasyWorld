0c3cf6c9e09819a880b3a8386b7623fb9eadcbd6
JiangWei <wei.jiang@winsuntech.cn>
2016-02-05 22:31:01 +0800

    modified.

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -14,12 +14,27 @@ bool MenuScene::init()
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
-    //auto black_scene = LayerColor::create();
-	//this->addChild(black_scene);
+	///////////////////////////////////
+    auto start_layer = Layer::create();
+    this->addChild(start_layer);
+
+    Sprite* my_start_bg = Sprite::create("material_6.png");
+    my_start_bg->setPosition(visibleSize.width/2,visibleSize.height/2);
+    my_start_bg->setOpacity(0);
+
+    auto fadeIn_my_start_bg = FadeIn::create(3.0f);
+    auto fadeOut_my_start_bg = FadeOut::create(3.0f);
+
+	start_layer->addChild(my_start_bg);
+
+    auto delay1 = DelayTime::create(13.0f);
+
+    auto seq2 = Sequence::create(delay1, fadeIn_my_start_bg, fadeOut_my_start_bg, nullptr);
+    my_start_bg->runAction(seq2);
 
     ///////////////////////////////////
-    //auto logo_scene = LayerColor::create();
-    //this->addChild(logo_scene);
+    /*auto logo_layer = LayerColor::create();
+    this->addChild(logo_layer);
 
     auto my_logo = Sprite::create("test2.png");
     my_logo->setPosition(visibleSize.width/2,visibleSize.height/2);
@@ -28,14 +43,14 @@ bool MenuScene::init()
     auto fadeIn_my_logo = FadeIn::create(3.0f);
     auto fadeOut_my_logo = FadeOut::create(3.0f);
 
-	this->addChild(my_logo,0);
+	logo_layer->addChild(my_logo,0);
 
     auto seq = Sequence::create(fadeIn_my_logo, fadeOut_my_logo, nullptr);
     my_logo->runAction(seq);
 
 	///////////////////////////////////
-    //auto thanks_scene = LayerColor::create();
-    //this->addChild(thanks_scene);
+    auto thanks_layer = LayerColor::create();
+    this->addChild(thanks_layer);
 
     auto my_thanks = Sprite::create("test.png");
     my_thanks->setPosition(visibleSize.width/2,visibleSize.height/2);
@@ -44,24 +59,23 @@ bool MenuScene::init()
     auto fadeIn_my_thanks = FadeIn::create(3.0f);
     auto fadeOut_my_thanks = FadeOut::create(3.0f);
 
-	this->addChild(my_thanks,0);
+	thanks_layer->addChild(my_thanks,0);
 
     auto delay = DelayTime::create(8.0f);
 
     auto seq1 = Sequence::create(delay, fadeIn_my_thanks, fadeOut_my_thanks, nullptr);
-    my_thanks->runAction(seq1);
-
-    CCLOG("hahahhahaha!");
-
-    //usleep(6000000);
+    my_thanks->runAction(seq1);*/
 
 
 
-    Director::getInstance()->pushScene(StartScene::create());
+
+
+
+    //Director::getInstance()->pushScene(StartScene::create());
 
     //Director::getInstance()->popScene();
 
-    //Director::getInstance()->replaceScene(StartScene::create());
+    Director::getInstance()->replaceScene(StartScene::create());
 
 	//this->addChild(MenuLayer::create());
 
