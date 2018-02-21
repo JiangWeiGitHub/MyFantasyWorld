f4d43012a147a7ed0a749430893adf613c587c2e
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2018-02-21 23:45:09 +0800

    new file:   Classes/Entity/Monster/Bird.cpp

---
+++ Classes/Entity/Monster/Bird.h
@@ -0,0 +1,37 @@
+#ifndef __HUMAN_H__
+#define __HUMAN_H__
+
+#include "../../../Global/Global.h"
+#include "../Role.h"
+
+namespace jiangweigithub {
+
+  class Human: public Role
+  {
+  public:
+    static Human* create();
+    static Human* create(const std::string &filename);
+    static Human* createWithSpriteFrame(cocos2d::SpriteFrame *spriteFrame);
+
+    bool setTalkContents(std::string contents);
+    std::string getTalkContents();
+
+    bool setSex(SEX sex);
+    SEX getSex();
+
+    bool setMood(MOOD mood);
+    MOOD getMood();
+
+    bool setMovePosition(cocos2d::Point position);
+    cocos2d::Point getMovePosition();
+
+  private:
+    std::string _talkContents;
+    SEX _sex;
+    MOOD _mood;
+    cocos2d::Point _position;
+  };
+  
+}
+
+#endif // __HUMAN_H__
