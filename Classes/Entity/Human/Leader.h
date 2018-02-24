e5c7de30bd3a3f7151cf19f01a70eb32b656fe55
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2018-02-24 23:39:45 +0800

    modified:   Classes/Entity/Human/Leader.h

--- Classes/Entity/Human/Leader.h
+++ Classes/Entity/Human/Leader.h
@@ -19,27 +19,27 @@ namespace jiangweigithub {
     bool setName(std::string name);
     std::string getName();
 
-    static cocos2d::Animation* getAnimationTop();
-    static cocos2d::Animation* getAnimationBottom();
-    static cocos2d::Animation* getAnimationLeft();
-    static cocos2d::Animation* getAnimationRight();
+    cocos2d::Animation* getAnimationTop();
+    cocos2d::Animation* getAnimationBottom();
+    cocos2d::Animation* getAnimationLeft();
+    cocos2d::Animation* getAnimationRight();
 
   private:
     // static bool createAnimation(std::string fileName);
 
-    static unsigned int HP;
-    static std::string heroName;
-    static std::string fileName;
+    unsigned int HP;
+    std::string heroName;
+    std::string fileName;
 
-    static cocos2d::Vector<cocos2d::SpriteFrame*> top;
-    static cocos2d::Vector<cocos2d::SpriteFrame*> bottom;
-    static cocos2d::Vector<cocos2d::SpriteFrame*> left;
-    static cocos2d::Vector<cocos2d::SpriteFrame*> right;
+    cocos2d::Vector<cocos2d::SpriteFrame*> top;
+    cocos2d::Vector<cocos2d::SpriteFrame*> bottom;
+    cocos2d::Vector<cocos2d::SpriteFrame*> left;
+    cocos2d::Vector<cocos2d::SpriteFrame*> right;
 
-    static cocos2d::Animation* animationTop;
-    static cocos2d::Animation* animationBottom;
-    static cocos2d::Animation* animationLeft;
-    static cocos2d::Animation* animationRight;
+    cocos2d::Animation* animationTop;
+    cocos2d::Animation* animationBottom;
+    cocos2d::Animation* animationLeft;
+    cocos2d::Animation* animationRight;
   };
   
 }
