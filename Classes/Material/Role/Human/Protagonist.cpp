998027f851821efb71a626279bb8fd739061c276
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2018-03-08 23:25:45 +0800

    modified:   Classes/Material/Role/Human/Protagonist.cpp

--- Classes/Material/Role/Human/Protagonist.cpp
+++ Classes/Material/Role/Human/Protagonist.cpp
@@ -2,31 +2,6 @@
 
 namespace jiangweigithub {
 
-  // cocos2d::Sprite* Protagonist::getProtagonistSprite()
-  // {
-  //   cocos2d::Texture2D* textTure = cocos2d::Director::getInstance()->getTextureCache()->addImage("sabin.png");
-
-  //   // right
-  //   cocos2d::SpriteFrame* frame1 = cocos2d::SpriteFrame::createWithTexture(textTure, cocos2d::Rect(180,0,30,50));
-  //   cocos2d::SpriteFrame* frame2 = cocos2d::SpriteFrame::createWithTexture(textTure, cocos2d::Rect(210,0,30,50));
-  //   cocos2d::SpriteFrame* frame3 = cocos2d::SpriteFrame::createWithTexture(textTure, cocos2d::Rect(240,0,30,50));
-  //   cocos2d::SpriteFrame* frame4 = cocos2d::SpriteFrame::createWithTexture(textTure, cocos2d::Rect(210,0,30,50));
-
-  //   cocos2d::Vector<cocos2d::SpriteFrame*> right;
-  //   right.pushBack(frame1);
-  //   right.pushBack(frame2);
-  //   right.pushBack(frame3);
-  //   right.pushBack(frame4);
-
-  //   auto loadingSprite = cocos2d::Sprite::createWithSpriteFrame(frame1);
-
-  //   auto animationRight = cocos2d::Animation::createWithSpriteFrames(right,0.2f);
-
-  //   loadingSprite->runAction(cocos2d::RepeatForever::create(cocos2d::Animate::create(animationRight)));
-
-  //   return loadingSprite;
-  // }
-
   Protagonist* Protagonist::create()
   {
     Protagonist *sprite = new Protagonist();
